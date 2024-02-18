7e38a2a42e6feb7ea7ad38c394b8b645374b7bbd
Jonjo Jefferson <jonjo.jefferson@gmail.com>
2024-02-18 18:41:12 +0000

    first enemy
    added enemy assets, basic movement

--- Main Game/main.py
+++ Main Game/main.py
@@ -1,5 +1,6 @@
 import pygame as pg
 import constants as c
+from enemy import Enemy
 
 # init game
 pg.init()
@@ -10,13 +11,35 @@ clock = pg.time.Clock()
 # window
 screen = pg.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
 pg.display.set_caption("Potato Defense")
+
+# load images
+enemy_image = pg.image.load(
+    "main game/assets/images/enemies/enemy_1.png"
+).convert_alpha()
+
+
+enemy = Enemy((200, 300), enemy_image)
+
+# groups
+enemy_group = pg.sprite.Group()
+enemy_group.add(enemy)
+
+print(enemy)
 # game loop
 run = True
 while run:
+
     clock.tick(c.FPS)
+
+    screen.fill("grey100")
+    # update groups
+    enemy_group.update()
+
+    # draw groups
+    enemy_group.draw(screen)
     for event in pg.event.get():
         if event.type == pg.QUIT:
             run = False
 
-
-pg.QUIT()
+    # update display
+    pg.display.flip()
